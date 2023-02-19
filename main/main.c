e1d29daca54d35cf4abc684a348348513eec8883
Richard Egeli <richard@Richards-MacBook-Pro.local>
2023-02-19 14:01:42 +0100

    feat: added transprt type flag that was missing, added authorization token

--- main/main.c
+++ main/main.c
@@ -13,26 +13,37 @@
 #include "sensor.h"
 #include "wifi.h"
 
+#define TAG "MAIN"
 #define CHECK_ERR(func) \
     if (func != ESP_OK) \
-        ESP_LOGE("MAIN", #func " failed, err: %d", func);
+        ESP_LOGE(TAG, #func " failed, err: %d", func);
 
 void send_data(char* body) {
     esp_http_client_config_t config = {
-        .host              = CONFIG_HTTP_HOST,
-        .path              = CONFIG_HTTP_PATH,
-        .port              = CONFIG_HTTP_PORT,
-        .method            = HTTP_METHOD_POST,
+        .host   = CONFIG_HTTP_HOST,
+        .path   = CONFIG_HTTP_PATH,
+        .port   = 443,
+        .method = HTTP_METHOD_POST,
+#ifdef CONFIG_HTTP_USE_SSL
+        .transport_type    = HTTP_TRANSPORT_OVER_SSL,
         .crt_bundle_attach = esp_crt_bundle_attach,
+#endif
     };
 
     esp_http_client_handle_t client = esp_http_client_init(&config);
+    if (client == NULL) {
+        ESP_LOGE(TAG, "Failed to initialize HTTP client");
+        return;
+    }
 
-    esp_http_client_set_header(client, "Content-Type", "application/json");
-    esp_http_client_set_post_field(client, body, strnlen(body, 2048));
+    if (strnlen(CONFIG_HTTP_TOKEN, 1024) > 0) {
+        esp_http_client_set_header(client, "Authorization", "Bearer " CONFIG_HTTP_TOKEN);
+    }
 
-    esp_http_client_perform(client);
-    esp_http_client_cleanup(client);
+    CHECK_ERR(esp_http_client_set_header(client, "Content-Type", "application/json"));
+    CHECK_ERR(esp_http_client_set_post_field(client, body, strnlen(body, 2048)));
+    CHECK_ERR(esp_http_client_perform(client));
+    CHECK_ERR(esp_http_client_cleanup(client));
 }
 
 char* read_data(void) {
@@ -56,15 +67,25 @@ void app_main(void) {
     CHECK_ERR(nvs_flash_init());
     CHECK_ERR(i2c_init());
     CHECK_ERR(sensor_init());
+    CHECK_ERR(wifi_start());
 
-    if (wifi_start() == ESP_OK && wifi_wait(10000) == ESP_OK) {
+    if (wifi_wait(10000) == ESP_OK) {
         char* body = read_data();
-        send_data(body);
-        free(body);
+        printf("Body: %s\n", body);
+
+        if (body != NULL) {
+            send_data(body);
+            free(body);
+        } else {
+            ESP_LOGE(TAG, "Sensor data body is NULL");
+        }
+    } else {
+        ESP_LOGE(TAG, "Failed to connect to WiFi");
     }
 
     CHECK_ERR(sensor_deinit());
     CHECK_ERR(i2c_deinit());
 
+    ESP_LOGI(TAG, "Going to sleep for %d seconds", CONFIG_DATA_INTERVAL);
     esp_deep_sleep(1000000LL * CONFIG_DATA_INTERVAL);
 }
